7ae6f27d36dbdcee38915d50a8adf6cd3f5973bb
Spencer Dent <sd00070@my.westga.edu>
2022-02-25 00:23:23 -0500

    set root page to ripples list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'ripples#index', as: 'ripples_index'
   resources :ripples
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
