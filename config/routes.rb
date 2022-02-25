f9b77114a96b11d98d431647cafb2d575c07ea4b
Spencer Dent <sd00070@my.westga.edu>
2022-02-25 12:19:02 -0500

    allow only valid ripple routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'ripples#index', as: 'ripples_index'
-  resources :ripples
+  resources :ripples, only: [:index, :show, :create, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
