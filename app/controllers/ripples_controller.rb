8ac6d6d52290c815c41d7dc10c75be1659e1e42e
Spencer Dent <sd00070@my.westga.edu>
2022-02-25 11:29:09 -0500

    move logic out of view into controller

--- app/controllers/ripples_controller.rb
+++ app/controllers/ripples_controller.rb
@@ -3,7 +3,7 @@ class RipplesController < ApplicationController
 
   # GET /ripples or /ripples.json
   def index
-    @ripples = Ripple.order(created_at: :desc)
+    @ripples = Ripple.order(created_at: :desc).slice(0, 10)
   end
 
   # GET /ripples/1 or /ripples/1.json
