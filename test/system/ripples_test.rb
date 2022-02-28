102a81c25b8b45bcd01df7b4d3f396e1b0b453b6
Spencer Dent <sd00070@my.westga.edu>
2022-02-27 23:29:27 -0500

    fix system tests

--- test/system/ripples_test.rb
+++ test/system/ripples_test.rb
@@ -3,45 +3,36 @@ require "application_system_test_case"
 class RipplesTest < ApplicationSystemTestCase
   setup do
     @ripple = ripples(:one)
+    @url = "https://thenumber#{rand(100)}iscool.com/"
   end
 
   test "visiting the index" do
     visit ripples_url
-    assert_selector "h1", text: "Ripples"
+    
+    assert_selector 'table'
+    assert_selector 'tr'
+    
+    assert_selector 'th', text: 'Name'
+    assert_selector 'th', text: 'Message'
+    assert_selector 'th', text: 'Posted'
+    
+    assert_text 'Newest | Previous 10 Ripples | Next 10 Ripples | Oldest | New Ripple'
   end
 
   test "creating a Ripple" do
     visit ripples_url
     click_on "New Ripple"
 
-    fill_in "Message", with: @ripple.message
-    fill_in "Name", with: @ripple.name
-    fill_in "Url", with: @ripple.url
+    fill_in 'ripple_name', with: @ripple.name
+    fill_in 'ripple_url', with: @url
+    fill_in 'ripple_message', with: @ripple.message
+    
     click_on "Create Ripple"
 
-    assert_text "Ripple was successfully created"
-    click_on "Back"
-  end
+    assert_selector '#notice', text: 'Ripple was successfully created'
 
-  test "updating a Ripple" do
-    visit ripples_url
-    click_on "Edit", match: :first
-
-    fill_in "Message", with: @ripple.message
-    fill_in "Name", with: @ripple.name
-    fill_in "Url", with: @ripple.url
-    click_on "Update Ripple"
-
-    assert_text "Ripple was successfully updated"
-    click_on "Back"
-  end
-
-  test "destroying a Ripple" do
-    visit ripples_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Ripple was successfully destroyed"
+    assert_selector 'td a', text: @ripple.name
+    assert_selector 'td', text: @ripple.message
+    assert_selector 'td a', text: 'Show'
   end
 end
