0bb140027ba3e4660bafa804ee9a11355fe4cc1d
Spencer Dent <sd00070@my.westga.edu>
2022-02-27 22:51:12 -0500

    fix broken & remove unused tests

--- test/controllers/ripples_controller_test.rb
+++ test/controllers/ripples_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class RipplesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @ripple = ripples(:one)
+    @url = "https://thenumber#{rand(100)}iscool.com/"
   end
 
   test "should get index" do
@@ -16,38 +17,22 @@ class RipplesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create ripple" do
-    skip
     assert_difference('Ripple.count') do
-      post ripples_url, params: { ripple: { message: @ripple.message, name: @ripple.name, url: @ripple.url } }
+      post ripples_url, params: {
+        ripple: {
+          message: @ripple.message,
+          name: @ripple.name,
+          url: @url
+        }
+      }
     end
 
-    assert_redirected_to ripple_url(Ripple.last)
+    assert_redirected_to ripples_index_url
   end
 
   test "should show ripple" do
     get ripple_url(@ripple)
     assert_response :success
-  end
-
-  test "should get edit" do
-    skip
-    get edit_ripple_url(@ripple)
-    assert_response :success
-  end
-
-  test "should update ripple" do
-    skip
-    patch ripple_url(@ripple), params: { ripple: { message: @ripple.message, name: @ripple.name, url: @ripple.url } }
-    assert_redirected_to ripple_url(@ripple)
-  end
-
-  test "should destroy ripple" do
-    skip
-    assert_difference('Ripple.count', -1) do
-      delete ripple_url(@ripple)
-    end
-
-    assert_redirected_to ripples_url
   end
 
   test "should validate url" do
